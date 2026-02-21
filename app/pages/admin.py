c0b60e00e9eed65cf383241ee2df760fee4110b8
James Fuller <fuller.jdf@outlook.com>
2026-02-21 15:00:20 -0600

    Add models to project. explanation in models.py

--- app/pages/admin.py
+++ app/pages/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from pages.models import AppUser
+from pages.models import AppUser, Room, Asset, Log, Task, Consumable
 
 admin.site.register(AppUser)
+admin.site.register(Room)
+admin.site.register(Asset)
+admin.site.register(Task)
+admin.site.register(Consumable)
+admin.site.register(Log)
