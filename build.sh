97208e18b15a80ca8ab902f16ce93a717ac37766
James Fuller <fuller.jdf@outlook.com>
2026-02-21 21:21:38 -0600

    fix render deployment path

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python app/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
-python app/manage.py migrate
+python manage.py migrate
