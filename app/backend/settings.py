0aa109a0ded0caebca0745d9b8302ee51ce83aa6
hamza <hamza@depaul.com>
2026-02-22 17:03:17 +0500

    local configs

--- app/backend/settings.py
+++ app/backend/settings.py
@@ -12,8 +12,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os, dj_database_url
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load .env from project root
+load_dotenv(BASE_DIR.parent / '.env')
 
 
 # Quick-start development settings - unsuitable for production
